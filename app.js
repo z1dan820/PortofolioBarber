b1a72bd5b91de09d63db2b9080b57dd6ba0f2448
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2025-04-12 08:27:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,22 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-list a');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('open');
+  hamburger.classList.toggle('open');
+});
+
+// Auto close menu saat link diklik
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('open');
+    hamburger.classList.remove('open');
+  });
+});
+
